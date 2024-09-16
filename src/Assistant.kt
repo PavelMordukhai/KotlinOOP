511aeb36efd19f5678fb4ea9e91d9543b818cd5e
paul_ <pavel.mordukhai@gmail.com>
2024-09-16 09:53:15 +0300

    OOP-04-Assistant

---
+++ src/Assistant.kt
@@ -0,0 +1,13 @@
+class Assistant {
+
+    fun bringCoffee() {
+        println("Get up")
+        println("Go to the coffee machine")
+        println("Press the \"Cappuccino\" button")
+        println("Wait for the cappuccino to be prepared")
+        println("Take coffee")
+        println("Bring coffee to the director")
+        println("Put coffee on the table")
+        println("Return to the workplace")
+    }
+}
