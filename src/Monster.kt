c8e0a89bd7f5794bdda3e28ed0a20a4e18420120
paul_ <pavel.mordukhai@gmail.com>
2024-09-16 21:52:50 +0300

    OOP-07 overloading constructor

--- src/Monster.kt
+++ src/Monster.kt
@@ -30,7 +30,13 @@ class Monster {
         size
     )
 
-    constructor() : this(Random.nextInt(1, 10))
+    constructor() : this(
+        Random.nextInt(1, 10),
+        Random.nextInt(1, 10),
+        Random.nextInt(1, 10),
+        Random.nextInt(1, 10),
+        Random.nextInt(1, 10),
+    )
 
     fun printInfo() {
         println(
@@ -47,12 +53,21 @@ class Monster {
 
 fun main() {
     val d = readln().split(" ").map { it.toInt() }
-    val monster = Monster(
+    val monster1 = Monster(
         d[0],
         d[1],
         d[2],
         d[3],
         d[4]
     )
-    monster.printInfo()
+    monster1.printInfo()
+    println()
+
+    val monster2 = Monster(5)
+    monster2.printInfo()
+    println()
+
+    val monster3 = Monster()
+    monster3.printInfo()
+    println()
 }
