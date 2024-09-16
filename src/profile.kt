23937b799133e19675b0366288ef08d0cbea2fd0
paul_ <pavel.mordukhai@gmail.com>
2024-09-16 09:29:44 +0300

    OOP-02-Person

--- src/profile.kt
+++ src/profile.kt
@@ -13,7 +13,19 @@ fun main() {
     print("Input 2st your age: ")
     second.age = readln().toInt()
 
-    println("\n1. Name: ${first.name}\nAge: ${first.age}")
-    println("\n2. Name: ${second.name}\nAge: ${second.age}")
+    print("Input 1st your height: ")
+    first.height = readln().toInt()
+    print("Input 2st your height: ")
+    second.height = readln().toInt()
+
+    print("Input 1st your weight: ")
+    first.weight = readln().toInt()
+    print("Input 2st your weight: ")
+    second.weight = readln().toInt()
+
+    println("\n1. Name: ${first.name}\nAge: ${first.age}" +
+            "\nHeight: ${first.height}\nWeight: ${first.weight}")
+    println("\n2. Name: ${second.name}\nAge: ${second.age}" +
+            "\nHeight: ${second.height}\nWeight: ${second.weight}")
 }
 
