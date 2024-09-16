61d42481c6fcbaa2ee9a3803077b783a17cc6aa1
paul_ <pavel.mordukhai@gmail.com>
2024-09-16 22:02:26 +0300

    OOP-08 primary constructor

--- src/Rectangle.kt
+++ src/Rectangle.kt
@@ -1,13 +1,7 @@
-class Rectangle {
-
-    val width: Int
+class Rectangle(
+    val width: Int,
     val height: Int
-
-    constructor(width: Int, height: Int) {
-        this.width = width
-        this.height = height
-    }
-
+) {
     constructor(size: Int): this(size, size)
 
     constructor(): this(1)
