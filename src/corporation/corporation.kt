2f8722098881df8c6f994775074edd8b8e167752
paul_ <pavel.mordukhai@gmail.com>
2024-09-17 09:18:23 +0300

    OOP-10 inheritance

--- src/corporation/corporation.kt
+++ src/corporation/corporation.kt
@@ -1,6 +1,12 @@
+package corporation
+
 fun main() {
 
     val director = Director("Andrey", 35)
     val consultant = Consultant("Max")
+    val assistant = Assistant("Helen", 20)
+
     director.getConsultantToWork(consultant)
+    println()
+    director.takeCoffee(assistant)
 }
