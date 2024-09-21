31f5f194514a836867ee60d00cfb847d386c96b0
paul_ <pavel.mordukhai@gmail.com>
2024-09-21 16:29:53 +0300

    OOP-15 mutable and files

---
+++ src/files/OperationCodeTest.kt
@@ -0,0 +1,8 @@
+package files
+
+enum class OperationCodeTest(val title: String) {
+
+    EXIT("Exit"),
+    ADD_NEW_ITEM("Add new item"),
+    SHOW_ALL_ITEMS("Show todo list")
+}
---
+++ src/files/WorkingWithFiles.kt
@@ -0,0 +1,52 @@
+package files
+
+import java.io.File
+
+fun main() {
+
+    val hw = File("test.txt")
+    hw.writeText("Hello ")
+    hw.appendText("World!\n\n")
+
+//    val toDoListFile = File("todo_list.txt")
+//
+//    while (true) {
+//        print("Enter a new item or 0 to exit: ")
+//
+//        val item = readln()
+//        if (item == "0")
+//            break
+//
+//        toDoListFile.appendText("$item\n")
+//    }
+//
+//    val content = toDoListFile.readText()
+//    println(content)
+
+    val operationCodes = OperationCodeTest.entries
+    val file = File("todo_list.txt")
+
+    while (true) {
+        print("Enter the operation code.")
+        for ((index, code) in operationCodes.withIndex()) {
+            print(" $index - ${code.title},")
+        }
+        print("\b: ")
+
+        val operationIndex = readln().toInt()
+        val operationCode = operationCodes[operationIndex]
+        when (operationCode) {
+            OperationCodeTest.EXIT -> break
+            OperationCodeTest.ADD_NEW_ITEM -> {
+                print("Enter a new item: ")
+                val item = readln()
+                file.appendText("$item\n")
+            }
+
+            OperationCodeTest.SHOW_ALL_ITEMS -> {
+                val content = file.readText()
+                println(content)
+            }
+        }
+    }
+}
