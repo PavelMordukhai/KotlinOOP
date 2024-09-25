5688869e892b20f375951fb8bed213834cee0c4f
paul_ <pavel.mordukhai@gmail.com>
2024-09-25 15:55:55 +0300

    OOP-21 abstract

--- src/corporation/FoodCard.kt
+++ src/corporation/FoodCard.kt
@@ -8,7 +8,7 @@ class FoodCard(
 ) : ProductCard(name, brand, price, ProductType.FOOD) {
 
     override fun printInfo() {
-        super.printInfo()
-        println(" Caloric: $caloric")
+        println("Name: $name Brand: $brand Price: $price " +
+                "Product type: ${productType.title} Wattage: $caloric")
     }
 }
